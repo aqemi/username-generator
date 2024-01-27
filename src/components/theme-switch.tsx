bce651e19811847f09108693dda87cc8b202d403
aqemi <2773683+aqemi@users.noreply.github.com>
2024-01-27 21:24:28 +0200

    base layout

---
+++ src/components/theme-switch.tsx
@@ -0,0 +1,40 @@
+import { clsx } from 'clsx';
+import { Moon, Sun } from 'lucide-react';
+import { useTheme } from 'next-themes';
+import { Fragment, useEffect, useState } from 'react';
+
+import { Button } from './ui/button';
+
+export const ThemeSwitch = () => {
+  const [mounted, setMounted] = useState(false);
+  const { setTheme, resolvedTheme } = useTheme();
+
+  const invertTheme = () => (resolvedTheme === 'light' ? setTheme('dark') : setTheme('light'));
+
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <Button variant="outline" size="icon" onClick={invertTheme}>
+      {mounted && (
+        <Fragment>
+          <Sun
+            className={clsx(
+              'clh-[1.2rem] w-[1.2rem] transition-all dark:rotate-0 dark:scale-100',
+              resolvedTheme === 'dark' ? 'rotate-0 scale-100' : 'rotate90 scale-0'
+            )}
+          />
+          <Moon
+            className={clsx(
+              'absolute h-[1.2rem] w-[1.2rem] transition-all dark:-rotate-90 dark:scale-0',
+              resolvedTheme === 'light' ? 'rotate-0 scale-100' : 'rotate90 scale-0'
+            )}
+          />
+        </Fragment>
+      )}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+};
