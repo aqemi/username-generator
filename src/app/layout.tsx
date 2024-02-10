63903bbce2a6995285005741e7b4ff8a60cbbc6f
aqemi <2773683+aqemi@users.noreply.github.com>
2024-02-10 22:29:15 +0200

    favicons, fonts, meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,48 @@
 import clsx from 'clsx';
 import type { Metadata } from 'next';
-import { Roboto_Mono } from 'next/font/google';
+import { Sono } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
 
 import './globals.css';
 
-const font = Roboto_Mono({ subsets: ['latin'] });
+const font = Sono({ weight: ['400', '700', '800'], subsets: ['latin'] });
 
+const title = 'Usernames Generator';
+const description =
+  'Generate unique and creative usernames effortlessly with our user-friendly username generator app. Discover personalized usernames for your online accounts and enhance your digital identity.';
 export const metadata: Metadata = {
-  title: 'Usernames Generator',
-  description:
-    'Generate unique and creative usernames effortlessly with our user-friendly username generator app. Discover personalized usernames for your online accounts and enhance your digital identity.',
+  title,
+  description,
+  icons: [
+    {
+      rel: 'icon',
+      sizes: 'any',
+      url: '/favicon.ico',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/favicon-16x16.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/apple-touch-icon.png',
+    },
+  ],
+  manifest: '/site.webmanifest',
+  openGraph: {
+    title,
+    description,
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
