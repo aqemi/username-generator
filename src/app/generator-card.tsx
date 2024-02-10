45fd48f388c9148dde3f2629f7cf1109840836c7
anonymous <anonymous@localhost>
2024-02-10 22:36:23 +0200

    uuid generator

--- src/app/generator-card.tsx
+++ src/app/generator-card.tsx
@@ -1,27 +1,38 @@
 'use client';
 
+import { SetStateAction, useState } from 'react';
 import { Generator } from '@/components/generator';
 import { Card, CardContent } from '@/components/ui/card';
 import { Copyable } from '@/components/ui/copyable';
 import { Separator } from '@/components/ui/separator';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { type UuidGeneratorResult } from '@/generators/uuid/result.interface';
+import { UuidGenerator } from '@/generators/uuid/uuid.generator';
 
 export interface GeneratorCardProps {
   uuid: UuidGeneratorResult;
 }
 
 export function GeneratorCard(props: GeneratorCardProps) {
+  const [state, setState] = useState(props);
+
+  const updateState = (newState: Partial<GeneratorCardProps>) => setState({ ...state, ...newState });
+
+  const regenerateUuid = async () => {
+    const result = await new UuidGenerator().generate();
+    updateState({ uuid: result });
+  };
+
   return (
     <Card className="w-full lg:w-[600px]">
       <CardContent className="p-4 space-y-4">
         <TooltipProvider>
-          <Generator title="Anime">
+          <Generator title="Anime" onRegenerate={() => {}}>
             <Copyable disabled type="text" value={'Nashimoto'} />
             <Copyable disabled type="text" value={'Nashimoto Keisuke'} />
           </Generator>
           <Separator></Separator>
-          <Generator title="Bible">
+          <Generator title="Bible" onRegenerate={() => {}}>
             <Copyable
               disabled
               type="text"
@@ -38,10 +49,10 @@ export function GeneratorCard(props: GeneratorCardProps) {
             />
           </Generator>
           <Separator></Separator>
-          <Generator title="Neutral"></Generator>
+          <Generator title="Neutral" onRegenerate={() => {}}></Generator>
           <Separator></Separator>
-          <Generator title="UUID">
-            <Copyable disabled type="text" value={props.uuid.v4} />
+          <Generator title="UUID" onRegenerate={regenerateUuid}>
+            <Copyable disabled type="text" value={state.uuid.v4} />
           </Generator>
         </TooltipProvider>
       </CardContent>
--- src/components/generator.tsx
+++ src/components/generator.tsx
@@ -6,12 +6,13 @@ import { Button } from '@/components/ui/button';
 
 interface GeneratorProps {
   title: string;
+  onRegenerate: () => any;
   children?: ReactNode;
 }
 
 const font = VT323({ weight: '400', subsets: ['latin'] });
 
-export function Generator({ title, children }: GeneratorProps) {
+export function Generator({ title, children, onRegenerate }: GeneratorProps) {
   return (
     <section className="flex items-center gap-4 group flex-wrap md:flex-nowrap">
       <h3 className={`${font.className} scroll-m-20 text-4xl tracking-tight min-w-[calc(100%_-_3.5rem)] md:min-w-32`}>
@@ -23,7 +24,7 @@ export function Generator({ title, children }: GeneratorProps) {
         className="flex-shrink-0 md:ml-auto md:order-last opacity-50"
         aria-label="Regenerate"
       >
-        <RefreshCcw className="h-4 w-4" />
+        <RefreshCcw className="h-4 w-4" onClick={onRegenerate} />
       </Button>
       {children}
     </section>
